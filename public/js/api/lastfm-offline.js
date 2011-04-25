13e68ef2e63324d5e5b7abda6f833a2e27bc9a32
Anton Podviaznikov <podviaznikov@gmail.com>
2011-04-25 10:06:34 +0200

    removed styles for jquery ui. add lastfm-offline.js

---
+++ public/js/api/lastfm-offline.js
@@ -0,0 +1,36 @@
+// m.Player
+// (c) 2011 Anton Podviaznikov, Enginimation Studio (http://enginimation.info).
+// m.player may be freely distributed under the MIT license.
+// For all details and documentation:
+// https://github.com/podviaznikov/m.player.
+"use strict";
+var apiUrl='http://ws.audioscrobbler.com/2.0/?';
+var apiKey='e3377f4b4d8c6de47c7e2c81485a65f5';
+
+var lastFM=
+{
+    getArtistImage:function(artist,callback)
+    {
+        callback('css/images/no_picture.png');
+    },
+
+    getAlbumImage:function(artist,album,callback)
+    {
+        callback('css/images/no_picture.png');
+    },
+
+    getAlbumPoster:function(artist,album,callback)
+    {
+       callback('css/images/no_picture.png');
+    },
+
+    getAlbumInfo:function(artist,album,callback)
+    {
+        var image='css/images/no_picture.png';
+        var albumName=album;
+        var releaseDate='na';
+        var songsCount='na';
+
+        callback({image:image,name:albumName,releaseDate:releaseDate,songsCount:songsCount});
+    }
+};
