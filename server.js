31219546dd943127cecc615cb474128ec8cb0456
Anton Podviaznikov <podviaznikov@gmail.com>
2011-05-05 22:59:39 +0200

    moved html/css/js into public folder.

--- server.js
+++ server.js
@@ -6,18 +6,18 @@ var util = require('util'),
 mime.define({'text/cache-manifest': ['mf']});
 app.configure(function()
 {
-    app.use(connect.favicon(__dirname + '/16.png'));
+    app.use(connect.favicon(__dirname + '/public/16.png'));
     //logger
     app.use(express.logger());
     //router
     app.use(app.router);
     //public folder for static files
-    app.use(express.static(__dirname));
+    app.use(express.static(__dirname+'/public'));
 });
 app.get('/app.mf', function(req, res)
 {
     res.contentType('text/cache-manifest');
-    res.sendfile(__dirname + '/app.mf');
+    res.sendfile(__dirname + '/public/app.mf');
 });
 app.listen(80);
 
