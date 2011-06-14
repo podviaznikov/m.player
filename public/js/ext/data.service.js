40832641ab8b46afafb6885456de46e3755815c3
Anton Podviaznikov <podviaznikov@gmail.com>
2011-06-14 22:40:45 +0200

    experiments.

--- public/js/ext/data.service.js
+++ public/js/ext/data.service.js
@@ -13,6 +13,7 @@ var dataService={
     {
         $.post('/song_played/'+artist+'/'+track+'/'+trackLength+'?user='+AppController.settings.getLastFmUser()
         +'&key='+AppController.settings.getLastFmSessionKey());
+        fbService.setStatus('Listening '+track);
     },
     getArtistImage:function(artist,callback){
         var jqxhr = $.get('/artist/'+artist+'/image',function(data){
@@ -56,6 +57,7 @@ var dataService={
     }
 };
 var fbService={
+    isLogined:false,
     init:function(){
         FB.init({
             appId  : '222066051151670',
@@ -63,25 +65,37 @@ var fbService={
             cookie : true, // enable cookies to allow the server to access the session
             xfbml  : false  // don't parse XFBML
         });
+        FB.Event.subscribe('auth.login',function(response){
+            fbService.isLogined=true;
+        });
     },
     login:function(callback){
-        FB.login(function(response){
-            if(response.session){
-                fbService.getUserName(callback);
-            }else{
-                callback('failed to login');
-            }
-        });
+        //get username if logined
+        if(fbService.isLogined){
+              fbService.getUserName(callback);
+        }
+        //login if not logined
+        else{
+            FB.login(function(response){
+                if(response.session){
+                    fbService.isLogined=true;
+                    fbService.getUserName(callback);
+                }
+                else{
+                    callback('failed to login');
+                }
+            });
+        }
     },
     logout:function(){
         FB.logout(function(response){
-            console.log('user was logout-ed')
+            fbService.isLogined=false;
         });
     },
     getUserName:function(callback){
         FB.api('/me', function(response){
           callback(undefined,response.name);
-       });
+        });
     },
     //change status of teh logined user to new one
     setStatus:function(status){
